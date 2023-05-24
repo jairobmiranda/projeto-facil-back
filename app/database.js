d18a0a423d0ea20a769a87a54c0ecfc9c31d21da
jairobmiranda <jairo17.mb@gmail.com>
2023-05-24 11:33:58 -0300

    atualização das dependencias

--- app/database.js
+++ app/database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
+  host: 'projetofacil.ddns.net',
   user: 'user_projeto',
   password: 'Entregador@1010',
   database: 'projeto_facil',
