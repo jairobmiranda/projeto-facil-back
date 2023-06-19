01d1357e49a8180d149e5f4cc46e8c04fd695eb9
Vitor Almeida <vitororama15@gmail.com>
2023-06-18 21:59:25 -0300

    correcao classe pessoa

--- app/atividade.js
+++ app/atividade.js
@@ -6,5 +6,45 @@ class Atividade extends CrudInterface {
     super('atividades');
   }
 }
+async iniciarAtividade(idPessoa, idAtividade) {
+    const pessoa = await this.findById(idPessoa);
+    if (!pessoa) {
+        return { 'msgErro': 'Pessoa não encontrada.' };
+    }
 
+    const atividade = await this.findById('atividades', idAtividade);
+    if (!atividade) {
+        return { 'msgErro': 'Atividade não encontrada.' };
+    }
+
+    const dataHoraInicio = new Date();
+    // Inserir um novo registro na tabela "atividade_pessoa" com os dados de início da atividade
+    const query = 'INSERT INTO atividade_pessoa (id_pessoa, id_atividade, hora_inicio) VALUES ($1, $2, $3)';
+    await pool.query(query, [idPessoa, idAtividade, dataHoraInicio]);
+
+    return { 'msgSucesso': 'Atividade iniciada com sucesso.' };
+}
+
+ async pararAtividade(idPessoa, idAtividade) {
+    const pessoa = await this.findById(idPessoa);
+    if (!pessoa) {
+        return { 'msgErro': 'Pessoa não encontrada.' };
+    }
+
+    const atividade = await this.findById('atividades', idAtividade);
+    if (!atividade) {
+        return { 'msgErro': 'Atividade não encontrada.' };
+    }
+
+    const dataHoraParada = new Date();
+    // Atualizar o campo "hora_fim" no registro correspondente na tabela "atividade_pessoa"
+    const query = 'UPDATE atividade_pessoa SET hora_fim = $1 WHERE id_pessoa = $2 AND id_atividade = $3';
+    await pool.query(query, [dataHoraParada, idPessoa, idAtividade]);
+
+    // Calcular a duração da atividade
+    const duracao = dataHoraParada - atividade.hora_inicio;
+
+    return { 'msgSucesso': 'Atividade parada com sucesso.', 'duracao': duracao };
+}
+}
 module.exports = Atividade;
--- app/pessoa.js
+++ app/pessoa.js
@@ -6,45 +6,19 @@ class Pessoa extends CrudInterface {
         super('pessoas');
     }
 
-    async iniciarAtividade(idPessoa, idAtividade) {
-        const pessoa = await this.findById(idPessoa);
-        if (!pessoa) {
-            return { 'msgErro': 'Pessoa não encontrada.' };
+    async login(req, res) {
+        try {
+            const { username, password } = req.body;
+            const query = `SELECT * FROM ${this.table} WHERE username = $1 AND password = $2`;
+            const { rows } = await pool.query(query, [username, password]);
+            if (rows.length === 0) {
+                res.status(401).json({ error: 'Credenciais inválidas.' });
+            } else {
+                res.json(rows[0]);
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Erro ao realizar o login.' });
         }
-
-        const atividade = await this.findById('atividades', idAtividade);
-        if (!atividade) {
-            return { 'msgErro': 'Atividade não encontrada.' };
-        }
-
-        const dataHoraInicio = new Date();
-        // Inserir um novo registro na tabela "atividade_pessoa" com os dados de início da atividade
-        const query = 'INSERT INTO atividade_pessoa (id_pessoa, id_atividade, hora_inicio) VALUES ($1, $2, $3)';
-        await pool.query(query, [idPessoa, idAtividade, dataHoraInicio]);
-
-        return { 'msgSucesso': 'Atividade iniciada com sucesso.' };
-    }
-
-    async pararAtividade(idPessoa, idAtividade) {
-        const pessoa = await this.findById(idPessoa);
-        if (!pessoa) {
-            return { 'msgErro': 'Pessoa não encontrada.' };
-        }
-
-        const atividade = await this.findById('atividades', idAtividade);
-        if (!atividade) {
-            return { 'msgErro': 'Atividade não encontrada.' };
-        }
-
-        const dataHoraParada = new Date();
-        // Atualizar o campo "hora_fim" no registro correspondente na tabela "atividade_pessoa"
-        const query = 'UPDATE atividade_pessoa SET hora_fim = $1 WHERE id_pessoa = $2 AND id_atividade = $3';
-        await pool.query(query, [dataHoraParada, idPessoa, idAtividade]);
-
-        // Calcular a duração da atividade
-        const duracao = dataHoraParada - atividade.hora_inicio;
-
-        return { 'msgSucesso': 'Atividade parada com sucesso.', 'duracao': duracao };
     }
 }
 
